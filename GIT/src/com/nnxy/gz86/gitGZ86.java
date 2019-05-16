fbbf173a20226006b088df678ab7a97682a6ce95
GZ86 <49502136+GZ86@users.noreply.github.com>
2019-05-16 09:43:06 +0800

    Update gitGZ86.java

--- GIT/src/com/nnxy/gz86/gitGZ86.java
+++ GIT/src/com/nnxy/gz86/gitGZ86.java
@@ -1,9 +1,9 @@
 package com.nnxy.gz86;
 
 public class gitGZ86 {
-	private int id;
-	private String username;
-	private String password;
+	private int id;//ID
+	private String username;//用户名
+	private String password;//密码
 	private int sex;
 	private String love;
 	public int getId() {
